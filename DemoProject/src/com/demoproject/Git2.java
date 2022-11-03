6a1b6a31e9b1e7b0a32a5981563f2010e4bf8a04
Tushar <tusharlokhande10@gmail.com>
2022-11-03 08:11:08 +0530

    Third commit

--- DemoProject/src/com/demoproject/Git2.java
+++ DemoProject/src/com/demoproject/Git2.java
@@ -5,5 +5,6 @@ public class Git2 {
 	public static void main(String[] args) {
 		System.out.println("Git trial 2");
 		System.out.println("Second commite");
+		System.out.println("Third trial");
 	}
 }
